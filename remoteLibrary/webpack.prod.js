4b6bada5615a3cc7810795fdafeb574581294dcb
Marco <marco.falcone@qontinual.com>
2023-08-29 16:46:41 +0200

    edit prod output

--- remoteLibrary/webpack.prod.js
+++ remoteLibrary/webpack.prod.js
@@ -4,11 +4,4 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'production',
   entry: './src/button.jsx',
-  output: {
-    filename: 'bundle.js',
-    library: {
-      type: 'commonjs',
-      name: "remoteLibrary",
-    },
-  },
 });
