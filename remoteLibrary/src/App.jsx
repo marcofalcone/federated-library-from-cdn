733f478bf60beae8add5bd66f26d0542024c248a
marcofalcone <mfalcone.dev@gmail.com>
2023-09-03 11:56:01 +0200

    test edit button

--- remoteLibrary/src/App.jsx
+++ remoteLibrary/src/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import Button from "./button"
+import Button from "./button";
 
 const App = () => {
   return (
-    <div className="h-screen w-screen bg-slate-800 flex justify-center">
-      <div className="mt-10">
+    <div className= "w-screen h-screen flex items-center flex-col bg-slate-800">
+      <h1 className="mb-36 mt-10 text-5xl text-white">Library</h1>
       <Button />
-      </div>
-    </div>
-  )
+    </div>)
 }
 
-export default App;
 
+export default App;
--- remoteLibrary/src/button.jsx
+++ remoteLibrary/src/button.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./index.css"
 
-const Button = () => <button className="m-3 w-40 h-20 rounded-lg border-2 border-sky-600 text-white hover:bg-sky-600 transition ease-linear hover:scale-105">Remote button</button>
+const Button = () => <button className="m-3 w-40 h-20 rounded-lg border-2 border-sky-600 text-white hover:bg-red-600 transition ease-linear hover:scale-105">Edit Remote button</button>
 
 export default Button
